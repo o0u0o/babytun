d386961a64a90d39b51c65bf871926b28260fb2e
o0u0o <1553122309@qq.com>
2023-07-08 19:34:37 +0800

    :sparkles:  新增Redis缓存

--- src/main/java/com/o0u0o/babytun/BabytunApplication.java
+++ src/main/java/com/o0u0o/babytun/BabytunApplication.java
@@ -3,9 +3,11 @@ package com.o0u0o.babytun;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.o0u0o.babytun")
+@EnableCaching //开启声明式缓存（利用注解来控制缓存读写）
 public class BabytunApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/o0u0o/babytun/service/GoodsService.java
+++ src/main/java/com/o0u0o/babytun/service/GoodsService.java
@@ -8,6 +8,7 @@ import com.o0u0o.babytun.entity.Goods;
 import com.o0u0o.babytun.entity.GoodsCover;
 import com.o0u0o.babytun.entity.GoodsDetail;
 import com.o0u0o.babytun.entity.GoodsParam;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -33,18 +34,29 @@ public class GoodsService {
     @Resource
     private GoodsParamDAO goodsParamDAO;
     //view -> controller -> service -> dao
+
+    /**
+     * 注解@Cacheable，是声明式缓存的核心注解
+     * 第一次访问的时候将方法的返回值缓存起来，下次访问直接从缓存中获取
+     * @param goodsId
+     * @return
+     */
+    @Cacheable(value = "goods", key = "#goodsId")
     public Goods getGoods(Long goodsId) {
         return goodsDAO.findById(goodsId);
     }
 
+    @Cacheable(value = "covers", key = "#goodsId")
     public List<GoodsCover> findCovers(Long goodsId){
         return goodsCoverDAO.findByGoodsId(goodsId);
     }
 
+    @Cacheable(value = "details", key = "#goodsId")
     public List<GoodsDetail> findDetails(Long goodsId){
         return  goodsDetailDAO.findByGoodsId(goodsId);
     }
 
+    @Cacheable(value = "params", key = "#goodsId")
     public List<GoodsParam> findParams(Long goodsId){
         List list =  goodsParamDAO.findByGoodsId(goodsId);
         return list;
